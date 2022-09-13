2fc091f43dd13f7a95a2cbab70c5e4aa16dae937
manoj-cloudcosmos <manoj-cloudcosmos@users.noreply.github.com>
2022-09-13 00:40:43 +0000

    jekyll build from Action f0134efc5dc65ba9d9583368304e41b0c3b22e44

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,49 @@
+var store = [{
+        "title": "Quick-Start Guide",
+        "excerpt":"NewsFetch has been developed with Python. To get started, you will need to have Python 3.9 or higher installed on your system. Python can be downloaded from here. Pyenv is a tool that can be used to manage multiple versions of Python on the same system. It can be installed...","categories": [],
+        "tags": [],
+        "url": "/docs/quick-start-guide/",
+        "teaser": null
+      },{
+        "title": "Extracting News From CommonCrawl",
+        "excerpt":"Introduction CommonCrawl is a non-profit organization that provides a free and open repository of web crawl data. It is a great source of data for NewsFetch. Let us go over the process of using NewsFetch to extract News data from CommonCrawl. The extracted data is further processed with NewsPlease. Common...","categories": [],
+        "tags": [],
+        "url": "/docs/extracting-news-commoncrawl/",
+        "teaser": null
+      },{
+        "title": "Enriching News Content",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/docs/enriching-news/",
+        "teaser": null
+      },{
+        "title": "API",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/docs/api/",
+        "teaser": null
+      },{
+        "title": "Ideas For Using NewsFetch",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/docs/app-ideas-newsfetch/",
+        "teaser": null
+      },{
+        "title": "Contributing",
+        "excerpt":"Having trouble working with code? Found a typo in the documentation? Interested in adding a feature or fixing a bug? Then by all means submit an issue or pull request. If this is your first pull request, it may be helpful to read up on the GitHub Flow first. NewsFetch...","categories": [],
+        "tags": [],
+        "url": "/docs/contributing/",
+        "teaser": null
+      },{
+        "title": "License",
+        "excerpt":"The MIT License (MIT) Copyright (c) 2022-2022 CloudCosmos and contributors Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish,...","categories": [],
+        "tags": [],
+        "url": "/docs/license/",
+        "teaser": null
+      },{
+        "title": "Building News applications with NewsFetch",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/building-news-apps-with-newsfetch/",
+        "teaser": null
+      }]
